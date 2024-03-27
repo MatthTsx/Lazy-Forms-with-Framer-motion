f0c0656441444daea25e0f1e0f9dcd46a7d58ff0
Mateus <shakii876954@gmail.com>
2024-03-27 05:13:07 -0300

    [fix]:<sizes>

--- src/layout/screens/Personal-2.tsx
+++ src/layout/screens/Personal-2.tsx
@@ -22,35 +22,35 @@ function Personal_2({...p}: CommonPropsForScreen) {
         top: useTransform(smooth, [k(140), k(100)], ["400%", "0%"])
     }}
     >
-        <motion.div className='absolute w-[460px] aspect-square rounded-full P2-Gradient-2 right-[27.5%] overflow-hidden' style={{
+        <motion.div className='absolute w-[33.67%] aspect-square rounded-full P2-Gradient-2 right-[27.5%] overflow-hidden' style={{
             bottom: useTransform(smooth, [k(110), k(90)], ["-100%","-22.5%"]),
             scale: useTransform(smooth, [k(78), k(50)], ["1", "5"])
         }}/>
-        <motion.div className='w-[518px] aspect-square rotate-45 -right-[10%] absolute bg-gradient-to-bl from-black to-[#261200]' style={{
+        <motion.div className='w-[39%] aspect-square rotate-45 -right-[10%] absolute bg-gradient-to-bl from-black to-[#261200]' style={{
             bottom: useTransform(smooth, [k(110), k(90)], ["-200%", "-40%"]),
             rotate: useTransform(smooth, [k(120), k(90)], ["-15deg", "45deg"]),
             background: useTransform(smooth, [k(78), k(53)], ["#000000","#FF810D"]),
             opacity: useTransform(smooth, [k(68), k(58)], ["1", "0"])
         }}/>
-        <motion.div className='w-[400px] aspect-square rotate-45 -left-[5%] absolute bg-gradient-to-tr from-black to-[#261200]' style={{
+        <motion.div className='w-[30%] aspect-square rotate-45 -left-[5%] absolute bg-gradient-to-tr from-black to-[#261200]' style={{
             bottom: useTransform(smooth, [k(110), k(90)], ["-200%", "-30%"]),
             rotate: useTransform(smooth, [k(120), k(90)], ["90deg", "45deg"]),
             background: useTransform(smooth, [k(78), k(53)], ["#000000","#FF810D"]),
             opacity: useTransform(smooth, [k(68), k(58)], ["1", "0"])
         }}/>
-        <motion.div className='w-[600px] aspect-square rotate-45 left-[30%] -z-10 absolute bg-gradient-to-br from-black to-[#261200]' style={{
-            bottom: useTransform(smooth, [k(120), k(85)], ["-200%", "-30%"]),
+        <motion.div className='w-[45%] aspect-square rotate-45 left-[30%] -z-10 absolute bg-gradient-to-br from-black to-[#261200]' style={{
+            bottom: useTransform(smooth, [k(120), k(90)], ["-200%", "-30%"]),
             background: useTransform(smooth, [k(78), k(50)], ["#000000","#FF810D"]),
         }}/>
         <motion.p className='absolute top-[50%] right-[50%] translate-x-[50%] translate-y-[-80%] font-Rubik_Microbe text-white text-[64px] opacity-75' style={{
             color: useTransform(smooth, [k(73), k(68)], ["#ffffff", "#000000"]),
             rotate: useTransform(smooth, [k(73), k(53)], ["0deg", "-30deg"]),
-            translateX: useTransform(smooth, [k(73), k(53)], ["50%", "-30%"]),
+            translateX: useTransform(smooth, [k(73), k(53)], ["50%", "-20%"]),
             translateY: useTransform(smooth, [k(73), k(53)], ["-80%", "-200%"]),
-            scale: useTransform(smooth, [k(56), k(53)], ["1", ".8"])
+            // scale: useTransform(smooth, [k(56), k(53)], ["1", ".8"])
         }}>Where r u from?</motion.p>
 
-        <motion.div className='w-[120%] h-48 absolute rotate-[-30deg] bottom-8 left-0'>
+        <motion.div className='w-[120%] h-[28%] absolute rotate-[-30deg] bottom-[10%] left-0 flex justify-end'>
             <motion.div className='bg-gradient-to-t from-[#040014] to-[#1C0800] h-full absolute' style={{width: useTransform(smooth, [k(50), k(40)], ["0", "100%"])}}/>
             <motion.div style={{
                 display: useTransform(smooth, [k(100), k(40), k(20)], ["none", "none", "flex"]),
