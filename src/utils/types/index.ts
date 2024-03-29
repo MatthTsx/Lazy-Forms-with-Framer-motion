28ac4fb2850ef3706cddd2c0891bd30fc37e6eb3
Mateus <shakii876954@gmail.com>
2024-03-29 15:00:04 -0300

    [feat]<Quality> Added new exported types for faster use and cleaner code

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -3,3 +3,21 @@ export interface CommonPropsForScreen{
     i: number;
     end: number
 }
+
+export interface CommonPropsData extends CommonPropsForScreen{
+    setData: React.Dispatch<React.SetStateAction<_Data_>>
+}
+export interface CommonPDataPlus extends CommonPropsData{
+    Data: _Data_
+}
+
+export interface _Data_{
+    Name: string
+    Email: string
+    M_Name: string
+    D_Name: string
+    College: string
+    Course: string
+    RG: string
+    CPF: string
+}
